b000252d30efb587ce5eaebc687666db261f8ec0
ProJulius <110142017+ProJulius@users.noreply.github.com>
2023-02-02 00:12:51 +0700

    Add files via upload

---
+++ DSA01001_XÂU NHỊ PHÂN KẾ TIẾP.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxx = 1e6+5;
+
+#define faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+
+vector<int> a(maxx,0); 
+
+void sinhnhiphan(string s)
+{
+    int i = s.size()-1;
+    while(i >= 0 && s[i] == '1')
+    { 
+        s[i] = '0';
+        i--;
+    }
+    if(i >= 0) s[i] = '1';
+    for(int i = 0 ; i < s.size() ; i++)
+    {
+        cout << s[i];
+    }
+    cout << "\n";
+}
+
+
+int main()
+{
+    faster;
+    int t;
+    cin >> t;
+    cin.ignore();
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        sinhnhiphan(s);
+    }
+}
